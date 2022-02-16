3d47c85e74261ca6016351d08b9960cf2d09ef4e
dezizlava <gabrakovi@abv.bg>
2022-02-17 00:44:56 +0200

    Added login, mapper, userDto, exceptions.

--- src/main/java/com/example/arenacinema_springproject/controllers/UserController.java
+++ src/main/java/com/example/arenacinema_springproject/controllers/UserController.java
@@ -1,8 +1,35 @@
 package com.example.arenacinema_springproject.controllers;
 
+import com.example.arenacinema_springproject.models.dto.UserResponseDTO;
+import com.example.arenacinema_springproject.models.entities.User;
+import com.example.arenacinema_springproject.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.modelmapper.ModelMapper;
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class UserController {
+
+    public static final String LOGGED = "logged";
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PostMapping("/login")
+    public UserResponseDTO login(@RequestBody User user, HttpSession session){
+        if (session.getAttribute(LOGGED)!=null){
+            System.out.println("You are already logged!");
+        }
+        String email = user.getEmail();
+        String password = user.getPassword();
+        User u = userService.login(email,password);
+        session.setAttribute(LOGGED, true);
+        UserResponseDTO dto = modelMapper.map(u, UserResponseDTO.class);
+        return dto;
+    }
 
 }
--- src/main/java/com/example/arenacinema_springproject/services/UserService.java
+++ src/main/java/com/example/arenacinema_springproject/services/UserService.java
@@ -1,7 +1,32 @@
 package com.example.arenacinema_springproject.services;
-
+import com.example.arenacinema_springproject.models.entities.User;
+import com.example.arenacinema_springproject.models.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import com.example.arenacinema_springproject.exceptions.BadRequestException;
+import com.example.arenacinema_springproject.exceptions.UnauthorizedException;
+
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User login(String email, String password){
+        if (email == null || email.isBlank()){
+            throw new BadRequestException("Email is mandatory!");
+        }
+        if (password == null || password .isBlank()){
+            throw new BadRequestException("Password is mandatory!");
+        }
+        User u  = userRepository.findByEmailAndPassword(email, password);
+        if (u==null){
+            throw new UnauthorizedException("Wrong data!");
+        }
+        return u;
+    }
 }
